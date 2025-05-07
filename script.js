b74f6d5bb350ccba2716ac0be82085407236481e
Luke Leheny <luke.leheny@pm.me>
2025-05-07 00:06:31 -0400

    Add more images, remove pixellation

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ const urls = [
   "https://d9-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/styles/full_width/public/thumbnails/image/drinking-water.jpg",
   "https://thewholeu.uw.edu/wp-content/uploads/water.png",
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTciJSt76D5K8s5IMkOuGplujvrvX5QpjbfLg&s",
+  "https://static.wikia.nocookie.net/fanon/images/6/65/Saurolophus_drinking-LBT2.webp",
+  "https://static.wikia.nocookie.net/the-land-before-time-dinopedia/images/5/53/Mr_Clubtail_drinking.webp",
+  "https://static.wikia.nocookie.net/spongebob/images/2/2c/Tea_at_the_Treedome_174.png",
+  "https://i.imgflip.com/24u0v8.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO5GPZc-lbkQtFA_1B6Elp9lvUnCUW88WzHw&s",
+  "https://media.tenor.com/os1SsnDLvSUAAAAe/pikachu-drinking.png",
+  "https://image.lexica.art/full_webp/27ff0524-e286-43ff-b84e-02fae1d9e603",
+  "https://images.pexels.com/photos/416528/pexels-photo-416528.jpeg",
+  "https://images.stockcake.com/public/b/a/4/ba483125-a815-4a61-bba9-381bb61784b0_medium/glass-of-water-stockcake.jpg",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5ERpJGoI29YhuSBwlpdquc1mGDe1vgYabRw&s",
+  "https://image.shutterstock.com/image-photo/water-pouring-into-glass-on-260nw-2498098055.jpg",
+  "https://previews.123rf.com/images/zzzz17/zzzz172303/zzzz17230300039/199680666-simple-yet-powerful-beauty-of-water-featuring-a-glass-of-water-sitting-on-a-table-the-glass-is.jpg",
 ];
 
 window.onload = () => {
